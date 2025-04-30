2b506eaae8b8670c9b942ebf7374fb772cf278b8
Denis Krasnov <dal.krasnov@sov-teh.com>
2025-04-30 16:01:46 +0300

    adding github static.yml

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/test-for-119681732",
+  base: "test-for-119681732",
   plugins: [vue()],
 })
 
