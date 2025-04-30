7c24a38068e7d707e2f8581f30e27df4731253b6
Denis Krasnov <dal.krasnov@sov-teh.com>
2025-04-30 16:10:02 +0300

    add base to vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/test-for-119681732/',
   plugins: [
     vue(),
     vueDevTools(),
